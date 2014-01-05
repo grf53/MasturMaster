67b161f2596416b0c174991d76c27ee2b2a36102
grf53 <grf53@naver.com>
2014-01-05 18:27:14 +0900

    Shaking Function(Actually Complete)

--- MasturTest/src/com/example/masturtest/MainActivity.java
+++ MasturTest/src/com/example/masturtest/MainActivity.java
@@ -8,6 +8,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -17,14 +18,13 @@ public class MainActivity extends Activity implements SensorEventListener {
 	TextView tvAX = null;
 	TextView tvAY = null;
 	TextView tvAZ = null;
-	// 방향 센서값을 출력하기 위한 TextView
-//	TextView tvOX = null;
-//	TextView tvOY = null;
-//	TextView tvOZ = null;
 	TextView tvTimes = null;
-	
+	TextView tvTime = null;
+	TextView tvDPM = null;
+		
 	int	Times = 0;
-	int[] pre = {0,0,0,0,0};
+	int[] pastAccel = {0,0,0,0,0};
+	long pastTime = 0, presentTime = 0;
 	
 	 PowerManager mPm;
      WakeLock mWakeLock;
@@ -33,9 +33,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 	SensorManager sm = null;
 	// 가속도 센서
 	Sensor accSensor = null;
-	// 방향 센서
-	Sensor oriSensor = null;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -46,13 +44,14 @@ public class MainActivity extends Activity implements SensorEventListener {
 		tvAY = (TextView) findViewById(R.id.tvAY);
 		tvAZ = (TextView) findViewById(R.id.tvAZ);
 		tvTimes = (TextView) findViewById(R.id.tvTimes);
+		tvTime = (TextView) findViewById(R.id.tvTime);
+		tvDPM = (TextView) findViewById(R.id.tvDPM);
 		
 		// SensorManager 인스턴스를 가져옴
 		sm = (SensorManager) getSystemService(SENSOR_SERVICE);
 		// 가속도 센서
 		accSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		// 방향 센서
-		oriSensor = sm.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+		pastTime = System.currentTimeMillis();
 	}
 	
 	@Override
@@ -60,15 +59,12 @@ public class MainActivity extends Activity implements SensorEventListener {
 		super.onResume();
 		// 가속도 센서 리스너 오브젝트를 등록
 		sm.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_GAME);
-		// 방향 센서 리스너 오브젝트를 등록
-		sm.registerListener(this, oriSensor, SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
 		// 센서에서 이벤트 리스너 분리
-		sm.unregisterListener(this);
 		sm.unregisterListener(this);
 	}
 
@@ -79,30 +75,28 @@ public class MainActivity extends Activity implements SensorEventListener {
 	}
 
 	@Override
-	public void onSensorChanged(SensorEvent event) {
+	public void onSensorChanged(SensorEvent event) {	
 		switch (event.sensor.getType()) {
 		case Sensor.TYPE_ACCELEROMETER:
 			tvAX.setText(String.valueOf(event.values[0]));
 			tvAY.setText(String.valueOf(event.values[1]));
 			tvAZ.setText(String.valueOf(event.values[2]));
-			pre[0] = pre[1];
-			pre[1] = pre[2];
-			pre[2] = pre[3];
-			pre[3] = pre[4];
-			pre[4] = (int) Math.sqrt(event.values[0]*event.values[0]+event.values[1]*event.values[1]+event.values[2]*event.values[2]);
-					
-			if(pre[0]<pre[1] && pre[1]<pre[2] && pre[2]>pre[3] && pre[3]>pre[4]){
-//				Log.d("gasokdo", ""+pre[1]);
+			pastAccel[0] = pastAccel[1];
+			pastAccel[1] = pastAccel[2];
+			pastAccel[2] = pastAccel[3];
+			pastAccel[3] = pastAccel[4];
+			pastAccel[4] = (int) Math.sqrt(event.values[0]*event.values[0]+event.values[1]*event.values[1]+event.values[2]*event.values[2]);
+			
+			Log.d("gasokdo", ""+pastAccel[2]);
+			presentTime = System.currentTimeMillis();
+			tvTime.setText((presentTime - pastTime)/1000+"."+(presentTime - pastTime)%1000+" s");
+			tvDPM.setText(String.format("%.3f", (60000*(Times/2)/((double)(presentTime - pastTime))))+" 딸/min");
+			if(pastAccel[0]<pastAccel[1] && pastAccel[1]<pastAccel[2] && pastAccel[2]>pastAccel[3] && pastAccel[3]>pastAccel[4]){
+				Log.d("_Times", ""+Times);
 				Times++;
-				tvTimes.setText((Times/2)+" times");
+				tvTimes.setText((Times/2)+" times");				
 			}
 			break;
-//		case Sensor.TYPE_GYROSCOPE:
-//			tvOX.setText(String.valueOf(event.values[0]));
-//			tvOY.setText(String.valueOf(event.values[1]));
-//			tvOZ.setText(String.valueOf(event.values[2]));
-//			break;
 		}
 	}
-
 }
